fdf2d24b34b398070ed263c2d1fdb677451362d0
kennygonzalezceballos2006 <kennyceballos2@gmail.com>
2026-02-25 21:42:41 -0500

    actualizacion

--- generador_codigos.py
+++ generador_codigos.py
@@ -1,16 +1,16 @@
-codigo = input("Ingrese el código del libro: ")
+while True:
+    codigo = input("Ingrese el código del libro: ")
 
-if not codigo.startswith("LIB-"):
-    partes = codigo.split("-", 1)
-    if len(partes) > 1:
-        codigo = "LIB-" + partes[1]
+    if not codigo.startswith("LIB-"):
+        partes = codigo.split("-", 1)
+        if len(partes) > 1:
+            codigo = "LIB-" + partes[1]
     else:
         codigo = "LIB-" + codigo
 
+    parte_restante = codigo[4:]
 
-parte_restante = codigo[4:]
-
-if len(parte_restante) == 10 or len(parte_restante) == 13:
-    print(f"Código válido: {codigo}")
-else:
-    print(f"Código inválido. La parte después de 'LIB-' debe tener 10 o 13 caracteres, actualmente tiene {len(parte_restante)}.")
+    if len(parte_restante) == 10 or len(parte_restante) == 13:
+        print(f"Código válido: {codigo}")
+    else:
+        print(f"Código inválido. La parte después de 'LIB-' debe tener 10 o 13 caracteres, actualmente tiene {len(parte_restante)}.")
--- registro_producto.py
+++ registro_producto.py
@@ -1,8 +1,9 @@
-categoria = input("Ingrese el nombre de la nueva categoría: ")
-
-if 5 <= len(categoria) <= 12:
-    
-    categoria_mayus = categoria.upper()
-    print(f"Categoría registrada exitosamente: {categoria_mayus}")
-else:
-    print("Nombre inválido. Debe tener entre 5 y 12 caracteres.")
+while True:#ciclo para repetir la pregunta hasta que se cumpla las condiciones
+    categoria = input("Ingrese el nombre de la nueva categoría: ")
+    if 5 <= len(categoria) <= 12:
+        categoria_mayus = categoria.upper()
+        print(f"Categoría registrada exitosamente: {categoria_mayus}")
+        print(f'el numero de letras es: {len(categoria)}')
+        break
+    else:
+        print("Nombre inválido. Debe tener entre 5 y 12 caracteres.")
